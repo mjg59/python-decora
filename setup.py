62acb59c828111bf3512a444be86fde0cda8c3ba
Matthew Garrett <mjg59@coreos.com>
2017-03-07 23:26:18 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ import warnings
 
 dynamic_requires = []
 
-version = 0.2
+version = 0.4
 
 setup(
     name='decora',
-    version=0.2,
+    version=0.4,
     author='Matthew Garrett',
     author_email='mjg59@srcf.ucam.org',
     url='http://github.com/mjg59/python-decora',
